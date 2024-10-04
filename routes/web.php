1e78081b6d0068a87c9cb35bc81ccb0e6133cdf1
mohamad dekmak <126089082+mohamadDekmak@users.noreply.github.com>
2024-10-04 04:20:08 +0300

    add tailwind

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{locale?}', function ($locale = null) {
     // Define your default locale
-    $defaultLocale = 'en'; // Set the default locale here
-
-    // Check if a locale was provided and if it is valid
+    $defaultLocale = 'ar'; // Set the default locale here
     if (isset($locale) && in_array($locale, config('app.available_locales'))) {
         app()->setLocale($locale);
     } else {
-        // If no valid locale is provided, set the default locale
         app()->setLocale($defaultLocale);
-        // Optionally redirect to the default locale URL
         return redirect("/$defaultLocale");
     }
 
