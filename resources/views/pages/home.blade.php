6dc63994690565daa2bab9a17df141abd6e091d6
mohamad dekmak <126089082+mohamadDekmak@users.noreply.github.com>
2024-10-04 03:36:38 +0300

    language_setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('welcome')
+@section('body')
+
+    <h1>{{__('test')}}</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{locale?}', function ($locale = null) {
+    // Define your default locale
+    $defaultLocale = 'en'; // Set the default locale here
+
+    // Check if a locale was provided and if it is valid
+    if (isset($locale) && in_array($locale, config('app.available_locales'))) {
+        app()->setLocale($locale);
+    } else {
+        // If no valid locale is provided, set the default locale
+        app()->setLocale($defaultLocale);
+        // Optionally redirect to the default locale URL
+        return redirect("/$defaultLocale");
+    }
+
+    return view('pages.home');
 });
